0672b49758fa0561aac6dd2a5e0e1248fd8fd0c7
uzlly <62786786+uzlly@users.noreply.github.com>
2020-06-11 01:31:49 +0700

    Add files via upload

---
+++ PublicationJournal.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PublicationJournal extends Model
+{
+    protected $table = 'publication_journals';
+
+    static $validation_rules = [
+        'id' => 'required',
+        'volume' => 'required'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'journal_name');
+    }
+
+    public function publication()
+    {
+        return $this->belongsTo(Publication::class, 'publication_id');
+    }
+}
