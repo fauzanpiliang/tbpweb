f2cc2679c7e9346075d346e6de1f86f0df564a30
hasyaronaa1711 <hasyarona1711@gmail.com>
2020-06-27 11:26:19 +0700

    revisi list publikasi

--- app/Http/Controllers/Backend/Publication/PublicationController.php
+++ app/Http/Controllers/Backend/Publication/PublicationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend\Publication;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use App\Models\Publication;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,13 @@ class PublicationController extends Controller
     {
         //mengambil data dari model publication
         $publications = Publication::all();
+        // $publications = DB::table('publications')
+        //                 ->join('publication_journals', 'publications.id', '=', 'publication_journals.publication_id')
+        //                 ->join('publication_proceedings', 'publications.id', '=', 'publication_proceedings.publication_id')
+        //                 ->join('lecturers', 'publications.lecturer_id', '=', 'lecturers.id')
+        //                 ->select('publications.title', 'publications.year', 'publications.type', 'publication_journals.journal_name', 'publication_journals.volume', 'publication_journals.no', 'publication_proceedings.proceeding_name', 'publication_proceedings.conference_name', 'publication_proceedings.conference_location', 'publication_proceedings.conference_date', 'lecturers.name')
+        //                 ->get();
+
         //mengirimkan data publikasi ke view index
         return view('klp08.index', compact('publications'));
     }
